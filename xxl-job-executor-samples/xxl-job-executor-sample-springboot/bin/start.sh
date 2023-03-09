110da53781057a19dfddb5a70e470c0f15d1992d
douchenglong <1623825495@qq.com>
2023-03-09 19:08:56 +0800

    改包名，改打包方式

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/bin/start.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+#jarName
+projectName=${project.build.finalName}
+target=dev
+port=${server.port}
+
+if [ -n "$1" ]; then
+   target=$1
+fi
+echo "target = "$target
+
+pid=`ps -ef |grep 'java' |grep $projectName |awk '{print $2}'`
+if [ -n "$pid" ]; then
+    kill -9 $pid
+    sleep 3
+fi
+DATE=`date +%Y-%m-%d`
+LogFileName=${HOME}/logs/$projectName
+
+if [ ! -d $LogFileName ]; then
+    mkdir -p $LogFileName
+fi
+
+case $target in
+    local)
+        JAVA_OPTS="-Xms2048m -Xmx2048m -Xss256k -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:SurvivorRatio=8"
+        JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$LogFileName/gc.log"
+        JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$LogFileName/dump"
+        JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=$target -Dspring.config.location=../conf/ -Dapollo_meta=$apollo_meta"
+        nohup java -jar $JAVA_OPTS ../boot/$projectName*.jar >> $LogFileName/start.$DATE.log 2>&1 &
+    ;;
+    dev)
+        JAVA_OPTS="-Xms2048m -Xmx2048m -Xss256k -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:SurvivorRatio=8"
+        JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$LogFileName/gc.log"
+        JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$LogFileName/dump"
+        JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=$target -Dspring.config.location=../conf/ -Dapollo_meta=$apollo_meta"
+        nohup java -jar $JAVA_OPTS ../boot/$projectName*.jar > $LogFileName/start.$DATE.log 2>&1 &
+    ;;
+    sit1 | sit3)
+        JAVA_OPTS="-Xms1024m -Xmx1024m -Xss256k -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:SurvivorRatio=8"
+        JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$LogFileName/gc.log"
+        JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$LogFileName/dump"
+        JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=$target -Dspring.config.location=../conf/ -Dapollo_meta=$apollo_meta"
+        nohup java -jar $JAVA_OPTS ../boot/$projectName*.jar > $LogFileName/start.$DATE.log 2>&1 &
+    ;;
+    uat)
+        JAVA_OPTS="-Xms1024m -Xmx1024m -Xss256k -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:SurvivorRatio=8"
+        JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$LogFileName/gc.log"
+        JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$LogFileName/dump"
+        JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=$target -Dspring.config.location=../conf/ -Dapollo_meta=$apollo_meta"
+        nohup java -jar $JAVA_OPTS ../boot/$projectName*.jar > $LogFileName/start.$DATE.log 2>&1 &
+    ;;
+    prod)
+        JAVA_OPTS="-Xms4096m -Xmx4096m -Xss256k -Xmn256m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -XX:SurvivorRatio=8"
+        JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$LogFileName/gc.log"
+        JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$LogFileName/dump"
+        JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=$target -Dspring.config.location=../conf/ -Dapollo_meta=$apollo_meta"
+        nohup java -jar $JAVA_OPTS ../boot/$projectName*.jar >> $LogFileName-start.$DATE.log 2>&1 &
+    ;;
+    *)
+        echo "No this ENV:$target."
+        exit 1
+    ;;
+esac
+
+  echo "Deploy Success"
+  exit 0
+
